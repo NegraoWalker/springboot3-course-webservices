8ae84715bd698b43966e256660fd5c54fbb8696a
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-04-11 10:34:07 -0300

    Aula 304

---
+++ src/main/java/com/walkernegrao/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.walkernegrao.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.walkernegrao.course.entities.Order;
+import com.walkernegrao.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
