d701680656136ba354f777546431a8703ef9c1a8
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-04-13 10:45:26 -0300

    Aula 310

--- src/main/java/com/walkernegrao/course/config/TestConfig.java
+++ src/main/java/com/walkernegrao/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.walkernegrao.course.entities.Category;
 import com.walkernegrao.course.entities.Order;
+import com.walkernegrao.course.entities.OrderItem;
 import com.walkernegrao.course.entities.Product;
 import com.walkernegrao.course.entities.User;
 import com.walkernegrao.course.entities.enums.OrderStatus;
 import com.walkernegrao.course.repositories.CategoryRepository;
+import com.walkernegrao.course.repositories.OrderItemRepository;
 import com.walkernegrao.course.repositories.OrderRepository;
 import com.walkernegrao.course.repositories.ProductRepository;
 import com.walkernegrao.course.repositories.UserRepository;
@@ -34,7 +36,8 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
-	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	
 	@Override
@@ -74,6 +77,14 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+	
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	
 	}
 	
 }
